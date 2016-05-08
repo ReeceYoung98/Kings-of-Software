45898b6fc045e5207b04a63f93a1500b4e1268b5
Reece Young <ReeceYoung98@gmail.com>
2016-05-08 22:51:47 +0100

    Basic skeleton
    Created basic website skeleton with login area.
    
    Based from design on Google Drive using Bootstrap rather than Google
    Chrome’s default login.

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+			</div>
+			<div class="footer navbar-fixed-bottom">
+				<div class="container">
+					<p class="text-muted">Copyright &copy; <?php echo date("Y"); ?>, Kings of Software. All rights reserved.</p>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,28 @@
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'includes/header.php';?>
+
+<div class="col-md-6 col-md-offset-3">
+	<form>
+		<h2 class="form-signin-heading">Please sign in</h2>
+
+		<div class="form-group">
+			<label for="inputEmail" class="sr-only">Email address</label>
+			<input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+		</div>
+
+		<div class="form-group">
+			<label for="inputPassword" class="sr-only">Password</label>
+			<input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+		</div>
+
+		<div class="checkbox">
+			<label>
+				<input type="checkbox" value="remember-me"> Remember me
+			</label>
+		</div>
+
+		<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+	</form>
+</div>
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'includes/footer.php';?>
