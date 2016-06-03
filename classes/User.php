24f548650ce69ac7793e02d8ae0e0b49a1d0d491
Reece Young <reeceyoung98@gmail.com>
2016-06-03 12:06:38 +0100

    Update for permissions

--- classes/User.php
+++ classes/User.php
@@ -92,8 +92,8 @@ class User{
 
 	public function hasPermission($key){
 		$group = $this->_db->get('Groups', array('Id', '=', $this->data()->Group));
-		print_r($group);
-		var_dump($group->count());
+		echo $group;
+		echo $group->count();
 		if ($group->count() === 1) {
 			$permissions = json_decode($group->first()->Permissions, true);
 
