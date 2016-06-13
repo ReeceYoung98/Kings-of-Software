91d38f55b4ae42200fb74e826d12f21d331e31cf
Reece Young <ReeceYoung98@gmail.com>
2016-06-13 13:21:00 +0100

    Removed sticky footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 			</div>
-			<div class="footer navbar-fixed-bottom">
+			<div class="footer">
 				<div class="container">
 					<p class="text-muted">Copyright &copy; <?php echo date("Y"); ?>, Kings of Software. All rights reserved.</p>
 				</div>
